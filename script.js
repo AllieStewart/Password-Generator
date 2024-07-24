34188a955d3c9357a054c985848732bb922214a7
AllieStewart <megaxis1217@gmail.com>
2024-07-24 12:50:49 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,112 +1,43 @@
 // Assignment code here
 // generatePassword() function in all of its glory
-function generatePassword()
-{
-  // genPW will be the password made from the user's input (how many chars, yes/no to symbols, lowercase, uppercase)
-  var genPW = [];
-  // finalPW will be the completed password based on the user's input
-  var finalPW = "";
-  // password to be returned from function
-  var password = "";
+function generatePassword() {
+  const characterSets = {
+    specSym: "!@#$%^&*()_+~`|{}[]:;?<>,./*-=",
+    nums: "0123456789",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  };
 
-  // Defining each of the elements that the user may or may not want
-  var specSym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "}", "{", "[", "]", ":", ";", "?", ">", "<", ",", ".", "/", "-", "="];
-  var nums = ["0","1","2","3","4","5","6","7","8","9"];
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-  // Using a windows prompt to ask how many characters the user would like
-  var numChar = window.prompt("How many characters would you like to have? (Must be between 8 - 128)");
-  // numChar; length must be <= 8 but <= 128
-  if (numChar >= 8 && numChar <= 128)
-  {
-    console.log("Number of characters: " + numChar)
-  }
-  // Requests firmly that the user enters a numerical amount between 8 - 128 if they didn't the first time
-  else 
-  {
-    console.log("Please enter between 8 - 128 characters: ");
-    numChar = window.prompt("Please enter between 8 - 128 characters: ");
+  // Prompt for password length
+  let numChar = prompt("How many characters would you like? (8-128)");
+  while (!numChar || numChar < 8 || numChar > 128) {
+    numChar = prompt("Please enter between 8 and 128 characters:");
   }
 
-  // Using a windows confirm screen to ask if user would like symbols or not
-  var includeSpecChars = window.confirm("Would you like to include symbols?");
-  // includeSpecChars; needs random amount of symbols if OK
-  if(includeSpecChars)
-  {
-    console.log("Special characters added.");
-    genPW = genPW.concat(specSym);
-  }
-  // Cancel
-  else
-  {
-    console.log("No special characters.");
+  // Gather character set preferences
+  const selectedSets = {};
+  for (const setName in characterSets) {
+    const include = confirm(`Include ${setName}?`);
+    selectedSets[setName] = include ? characterSets[setName] : "";
   }
 
-  // Using a windows confirm screen to ask if user would like numbers or not
-  var includeNumbers = window.confirm("Would you like to include numbers?");
-  // includeNumbers; needs random amount of numbers if OK
-  if(includeNumbers)
-  {
-    console.log("Numbers added.");
-    genPW = genPW.concat(nums);
-  }
-  // Cancel
-  else
-  {
-    console.log("No numbers.");
+  // Check if at least one set is selected
+  if (Object.values(selectedSets).every(set => set === "")) {
+    alert("Must choose at least one character set!");
+    return; // Abort if no sets selected
   }
 
-  // Using a windows confirm screen to ask if user would like lowercase letters or not
-  var includeLowercase = window.confirm("Would you like to include lowercase letters?");
-  // includeLowercase; needs random amount of lowercase letters if OK
-  if(includeLowercase)
-  {
-    console.log("Lowercase letters added.");
-    genPW = genPW.concat(lowerCase);
-  }
-  // Cancel
-  else
-  {
-    console.log("No lowercase letters.");
+  // Combine selected sets into a single string
+  const allChars = Object.values(selectedSets).join("");
+
+  // Generate password
+  let password = "";
+  for (let i = 0; i < numChar; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
   }
 
-  // Using a windows confirm screen to ask if user would like uppercase letters or not
-  var includeUppercase = window.confirm("Would you like to include uppercase letters?");
-  // includeUppercase; needs random amount of uppercase letters if OK
-  if(includeUppercase)
-  {
-    console.log("Uppercase letters added.");
-    genPW = genPW.concat(upperCase);
-  }
-  // Cancel
-  else
-  {
-    console.log("No uppercase letters.");
-  }
-
-  // Generate password based on criteria and add into "password" to return
-  for(var i = 0; i < numChar; i++)
-  {
-    finalPW = finalPW.concat(genPW[Math.floor(Math.random() * genPW.length)]);
-    password += finalPW[i];
-  }
-
-  // Checks to see if one of these options were chosen (MUST HAVE AT LEAST ONE)
-  if (includeSpecChars || includeLowercase || includeUppercase || includeNumbers)
-  {
-    console.log("Password meets criteria.");
-    // Return password
-    console.log(password);
-    return password;
-  }
-
-  // Will have to start process over if user hit 'Cancel' on all options after entering number of characters
-  else
-  {
-    console.log("Password needs at least one of: symbols, numbers, lowercase, or uppercase.");
-    alert("Password needs at least one of: symbols, numbers, lowercase, or uppercase.");
-  }
+  return password;
 }
 
 // Code already here 
